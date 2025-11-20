2755d8c28f9b44864b2f8d2a14bf71f03c6e499b
alexander-huynh <alexander.huynh@bellevuecollege.edu>
2025-11-19 16:40:04 -0800

    added first 3 sketches to proper position in page

--- js/sketches/sketch_renderer.js
+++ js/sketches/sketch_renderer.js
@@ -29,8 +29,23 @@
         draw: function (p, manager, ai, progress) {
             try { console.log('Renderer: delegating draw, ai=', ai); } catch (e) { }
 
-            if (ai === 0 || ai === 1) {
+            if (ai === 0) {
                 window.VizTitle.draw(p, manager, ai, progress);
+                return;
+            }
+
+            if (ai === 1) { // Fuel Type vs CO2
+                window.VizBar3.draw(p, manager, ai, progress);
+                return;
+            }
+
+            if (ai === 2) { // Engine Size vs CO2
+                window.VizScatter.draw(p, manager, ai, progress);
+                return;
+            }
+
+            if (ai === 3) { // Engine Size vs CO2
+                window.VizScatter2.draw(p, manager, ai, progress);
                 return;
             }
 
